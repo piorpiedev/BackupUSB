473322638e1ba8cdc3121ad18680ee5ff55890ae
piorpiedev <contact@piorpie.com>
2025-01-08 01:09:21 +0100

    Added debug information with how many files and folders have been archived/extracted at the end of execution

--- archive/tar.go
+++ archive/tar.go
@@ -9,16 +9,17 @@ import (
 	"strings"
 )
 
-func Tar(paths []string, out io.Writer) error {
-	tarball := tar.NewWriter(out)
-	defer tarball.Close()
+func Tar(paths []string, out io.Writer) (files, folders uint64, err error) {
+	tarWriter := tar.NewWriter(out)
+	defer tarWriter.Close()
+	files, folders = 0, 0
 
 	for _, fpath := range paths {
 		fpath = filepath.Clean(fpath)
 
 		info, err := os.Stat(fpath)
 		if err != nil {
-			return err // File does not exist or is inaccessible
+			return files, folders, err // File does not exist or is inaccessible
 		}
 
 		var baseDir string
@@ -40,23 +41,25 @@ func Tar(paths []string, out io.Writer) error {
 					fileHeader.Name = filepath.Join(baseDir, strings.TrimPrefix(path, fpath))
 				}
 
-				if err := tarball.WriteHeader(fileHeader); err != nil {
+				if err := tarWriter.WriteHeader(fileHeader); err != nil {
 					return err
 				}
 
 				if info.IsDir() {
+					folders++
 					fmt.Println("+ " + fileHeader.Name)
 					return nil
 				}
 
 				fmt.Printf("+ [%s] %s\n", FormatByteCount(info.Size()), fileHeader.Name)
+				files++
 				file, err := os.Open(path)
 				if err != nil {
 					return err
 				}
 				defer file.Close()
 
-				_, err = io.Copy(tarball, file)
+				_, err = io.Copy(tarWriter, file)
 				if err != nil {
 					return err
 				}
@@ -64,21 +67,22 @@ func Tar(paths []string, out io.Writer) error {
 			})
 
 		if err != nil {
-			return err
+			return files, folders, err
 		}
 	}
-	return nil
+	return files, folders, nil
 }
 
-func Untar(in io.Reader, out string) error {
+func Untar(in io.Reader, out string) (files, folders uint64, err error) {
 	tarReader := tar.NewReader(in)
+	files, folders = 0, 0
 
 	for {
 		header, err := tarReader.Next()
 		if err == io.EOF {
 			break
 		} else if err != nil {
-			return err
+			return files, folders, err
 		}
 
 		path := filepath.Join(out, header.Name)
@@ -86,22 +90,24 @@ func Untar(in io.Reader, out string) error {
 
 		if info.IsDir() {
 			fmt.Println("+ " + header.Name)
+			folders++
 			if err = os.MkdirAll(path, info.Mode()); err != nil {
-				return err
+				return files, folders, err
 			}
 			continue
 		}
 
 		fmt.Printf("+ [%s] %s\n", FormatByteCount(info.Size()), header.Name)
+		files++
 		file, err := os.OpenFile(path, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, info.Mode())
 		if err != nil {
-			return err
+			return files, folders, err
 		}
 		defer file.Close()
 		_, err = io.Copy(file, tarReader)
 		if err != nil {
-			return err
+			return files, folders, err
 		}
 	}
-	return nil
+	return files, folders, nil
 }
