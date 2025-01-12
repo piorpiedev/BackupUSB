68264db50add4dd2d08242fbbdfcad93f2a648e7
piorpiedev <contact@piorpie.com>
2025-01-12 13:21:11 +0100

    Fixed to compile for linux/amd64

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ echo Compiling for windows/arm64
 go build -ldflags "-s -w" -o bin/backup.exe .
 
 echo Compiling for linux/arm64
-set "GOOS=linux" & go build -ldflags "-s -w" -o bin/backup .
+set "GOOS=linux" & set "GOARCH=amd64" & go build -ldflags "-s -w" -o bin/backup .
 
 echo Stripping username
 py stripBinaries.py backup
